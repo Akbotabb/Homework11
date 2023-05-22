29f5a9bf71679f6d0abb058bbe85cb8d8ce2c5e6
Akbota <omarbek.akbota@gmail.com>
2023-05-22 20:25:27 +0600

    new changess

--- Homework11/ViewController.swift
+++ Homework11/ViewController.swift
@@ -7,14 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+class ViewController: UIViewController {
+    
+    //MARK: - UI
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "somet"
+        label.font = .systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: -Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("")
+    }
+    
+    //MARK: - Setup Views
+    private func setupViews() {
+        view.backgroundColor = .systemGreen
+        view.addSubview(label)
+    }
+    
+    //MARK: - Setup Constraints
+    
+    private func setupConstraints() {
+        
+    }
 }
+
+
 
